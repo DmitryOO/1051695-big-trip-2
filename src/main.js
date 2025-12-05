e2125837087ab2be16ad9bc9c74bb4467ca97fd2
DmitryOO <dimkut@mail.ru>
2025-12-05 21:44:58 +0400

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import SortView from './view/sort-view.js';
 import FilterView from './view/filter-view.js';
-import {render} from './render.js';
+import PointView from './view/point-view.js';
+import PointListView from './view/point-list-view.js';
+import EventEditView from './view/event-edit-view.js';
+import { render } from './render.js';
 
 const tripControls = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
-render(new SortView, tripEvents);
+// const pointListView = render(new PointListView, tripEvents);
 render(new FilterView, tripControls);
+render(new SortView, tripEvents);
+render(new PointListView, tripEvents);
+render(new EventEditView, tripEvents);
+for (let i = 0; i < 3; i++) {
+  render(new PointView, tripEvents);
+}
