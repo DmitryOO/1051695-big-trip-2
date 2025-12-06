0dd48146f792ba6908ff588248e6c73704e15754
DmitryOO <dimkut@mail.ru>
2025-12-06 17:34:41 +0400

    add presenter

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
-import SortView from './view/sort-view.js';
-import FilterView from './view/filter-view.js';
-import PointView from './view/point-view.js';
-import PointListView from './view/point-list-view.js';
-import EventEditView from './view/event-edit-view.js';
-import { render } from './render.js';
+import BoardPresenter from './presenter.js';
 
-const tripControls = document.querySelector('.trip-controls__filters');
-const tripEvents = document.querySelector('.trip-events');
-// const pointListView = render(new PointListView, tripEvents);
-render(new FilterView, tripControls);
-render(new SortView, tripEvents);
-render(new PointListView, tripEvents);
-render(new EventEditView, tripEvents);
-for (let i = 0; i < 3; i++) {
-  render(new PointView, tripEvents);
-}
+const filtersContainer = document.querySelector('.trip-controls__filters');
+const eventsContainer = document.querySelector('.trip-events');
+const boardPresenter = new BoardPresenter({ tripControls: filtersContainer, tripEvents: eventsContainer });
+boardPresenter.init();
