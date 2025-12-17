05adca651407ac7aec5f6ed0464147a0a554391c
DmitryOO <dimkut@mail.ru>
2025-12-17 23:59:59 +0400

    fix2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import BoardPresenter from './presenter/board-presenter';
 import FilterView from './view/filter-view.js';
 import { render } from './render.js';
+import PointModel from './model/point-model.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter(eventsContainer);
+const pointModel = new PointModel();
+const boardPresenter = new BoardPresenter(eventsContainer, pointModel);
 
+pointModel.init();
 render(new FilterView(), filtersContainer);
-
-
 boardPresenter.init();
 
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,22 +1,21 @@
 import SortView from '../view/sort-view.js';
+
 import PointView from '../view/point-view.js';
 import PointListView from '../view/point-list-view.js';
 import EditPointView from '../view/edit-point-view.js';
 import { render, RenderPosition } from '../render.js';
-import PointModel from '../model/point-model.js';
 import { getDefaultPoint } from '../utils.js';
 
 
 export default class BoardPresenter {
 
-  constructor(tripEvents) {
+  constructor(tripEvents, pointModel) {
     this.tripEvents = tripEvents;
-    this.pointModel = new PointModel();
+    this.pointModel = pointModel;
     this.pointListView = new PointListView();
   }
 
   init() {
-    this.pointModel.init();
     const points = this.pointModel.getPoints();
     const destinations = this.pointModel.getDestinations() || [];
     const offers = this.pointModel.getOffers();
