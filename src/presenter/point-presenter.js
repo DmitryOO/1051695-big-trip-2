3ac5b1c6c45f3066b6763b3cb04ee75d58e81e0b
DmitryOO <dimkut@mail.ru>
2026-01-05 20:30:50 +0400

    5.1.3

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,6 +1,6 @@
 import PointView from '../view/point-view';
 import EditPointView from '../view/edit-point-view';
-import { render, replace } from '../framework/render';
+import { render, replace, remove } from '../framework/render';
 
 export default class PointPresenter {
   #pointComponent = null;
@@ -18,10 +18,35 @@ export default class PointPresenter {
     this.#point = point;
     this.#destinations = destinations;
     this.#offers = offers;
+
+    const prevPointComponent = this.#pointComponent;
+    const prevEditPointComponent = this.#editPointComponent;
+
+
     this.#pointComponent = new PointView(this.#point, this.#destinations, this.#offers, this.#onRollupBtnPointClick);
     this.#editPointComponent = new EditPointView(this.#point, this.#destinations, this.#offers, this.#onRollupBtnFormClick, this.#onSaveBtnClick);
 
-    render(this.#pointComponent, this.#pointsContainer.element);
+    if (prevPointComponent === null || prevEditPointComponent === null) {
+      render(this.#pointComponent, this.#pointsContainer.element);
+      return;
+    }
+
+    if (this.#pointsContainer.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#pointsContainer.contains(prevEditPointComponent.element)) {
+      replace(this.#editPointComponent, prevEditPointComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevEditPointComponent);
+  }
+
+
+  destroy() {
+    remove(this.#pointComponent);
+    remove(this.#editPointComponent);
   }
 
   #onEscKeydown = (evt) => {
